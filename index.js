d3bd0d872879ddeebc42672ae4acb2ab1c47b8c0
Adam Mannino <amannino92@gmail.com>
2023-12-28 07:13:44 -0500

    Completes Modifying DOM Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove("main#main");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "ADAM-MANNINO is the champion";
